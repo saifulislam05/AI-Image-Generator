b0e3d1dec5a1f67390c00ea252b0a9acd301ce6e
saifulislam05 <saiful.bijulia@gmail.com>
2024-01-08 10:33:17 +0530

    desing done

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Body = () => {
+  // const [inputText, setInputText] = useState("");
+  // const [state, setstate] = useState(initialState);
+
+
   
   return (
-    <div className="w-11/12 mx-auto mt-4 md:mt-16">
-      
+    <div className="w-11/12 md:w-8/12 lg:w-6/12 mx-auto mt-4 ">
+      <h1 className="text-center text-xl mb-4 font-semibold uppercase">
+        Generate your desire Image
+      </h1>
+      <div className="w-full flex flex-col md:flex-row gap-3 mx-auto">
+        <input
+          type="text"
+          placeholder="Enter prompt to generate image"
+          className="input input-primary w-full"
+          // value={inputText}
+          // onChange={handleInputChange}
+        />
+        <button className="btn btn-primary" >Generate</button>
+      </div>
+      <div className=" w-full mt-6 rounded-xl overflow-hidden">
+        <div className="bg-primary text-primary-content py-2">
+        <h1 className="text-center text-lg font-semibold ">Generated Image</h1>
+        </div>
+        <img
+          src="https://images.pexels.com/photos/443446/pexels-photo-443446.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt="name"
+          className="w-full"
+        />
+      </div>
     </div>
   );
 };
