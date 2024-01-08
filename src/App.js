20452981361a57a25932da212849492ce8ba4605
saifulislam05 <saiful.bijulia@gmail.com>
2024-01-08 09:24:35 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Body from "./components/Body/Body";
+import Header from "./components/Header/Header";
+
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Body/>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Body/Body.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Body = () => {
+  
+  return (
+    <div className="w-11/12 mx-auto mt-4 md:mt-16">
+      
+    </div>
+  );
+};
+
+export default Body;
